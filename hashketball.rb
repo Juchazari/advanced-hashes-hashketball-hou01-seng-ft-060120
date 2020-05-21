fb531ceebf5cc2740ce2a803d327c4133560159b
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 22:26:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,6 +128,13 @@ def game_hash
 end
 
 def num_points_scored(player_name)
-  game_hash
-  binding.pry
+
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      data.each do |data_item|
+          binding.pry
+      end
+    end
+  end
+
 end
